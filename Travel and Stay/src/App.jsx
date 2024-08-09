21fef59f9417b8eaa8fd06b6d98936ec84b0f60c
clairemuiru1 <muiruclaire@gmail.com>
2024-08-09 19:32:52 +0300

    "completed home page "

--- Travel and Stay/src/App.jsx
+++ Travel and Stay/src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <>
       <Navbar/>
-      {/* <Home/>
-      <Blog/> */}
+      <Home/>
+      {/* <Blog/> */}
     </>
   );
 }
--- Travel and Stay/src/components/home/Home.jsx
+++ Travel and Stay/src/components/home/Home.jsx
@@ -3,9 +3,36 @@ import './home.css'
 
 function Home() {
   return (
-    <div>
-      Home
+   <section className='home'>
+    <div className="setContainer container">
+      <div className="homeText">
+        <h1 className='title'>
+          Plan your Travel with Habibi Travel
+        </h1>
+        <p className="subTitle">
+          Travel to your favourite city with respectful of the environment!
+        </p>
+        <button className="btn">
+          <a href="a">Explore Now</a>
+        </button>
+      </div>
+      <div className="homeCard grid">
+        <div className="locationDiv">
+          <label htmlFor="location">location</label>
+          <input type="text" placeholder='Dream Destination' />
+        </div>
+        <div className="distanceDiv">
+          <label htmlFor="distance">location</label>
+          <input type="text" placeholder='11/Meters' />
+        </div>
+        <div className="priceDiv">
+          <label htmlFor="price">location</label>
+          <input type="text" placeholder='$140- $500' />
+        </div>
+        <button className='btn'>Search</button>
+      </div>
     </div>
+   </section>
   )
 }
 
